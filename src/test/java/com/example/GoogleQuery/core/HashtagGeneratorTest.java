62e933202589445714964e9010dd315de8ae490e
erica <u11032030@lssh.tp.edu.tw>
2025-12-14 17:40:32 +0800

    data

--- src/test/java/com/example/GoogleQuery/core/HashtagGeneratorTest.java
+++
@@ -1,5 +0,0 @@
-package com.example.GoogleQuery.core;
-
-public class HashtagGeneratorTest {
-    
-}
--- src/test/java/com/example/GoogleQuery/integration/SearchEngineIntegrationTest.java
+++
@@ -1,5 +0,0 @@
-package com.example.GoogleQuery.integration;
-
-public class SearchEngineIntegrationTest {
-    
-}
